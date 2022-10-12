7247d140f1a50311b27f0cc2704b86827209c652
shaunkenclark <10130616+shaunkenclark@users.noreply.github.com>
2022-10-12 14:43:56 +0100

    working on it

--- build_view.sh
+++ build_view.sh
@@ -2,17 +2,17 @@
 
 cd build
 
-# grep -rli '/stylesheets/manifest.css' * | xargs -i@ sed -i 's/\/stylesheets\/manifest.css/\/hot-playboook\/stylesheets\/manifest.css/g' @
+# grep -rli '/stylesheets/manifest.css' * | xargs -i@ sed -i 's/\/stylesheets\/manifest.css/\/hot-playbook\/stylesheets\/manifest.css/g' @
 
-grep -rli '/stylesheets/manifest.css' * | xargs -i@ sed -i 's/\/stylesheets\/manifest.css/\/hot-playboook\/stylesheets\/manifest.css/g' @
+grep -rli '/stylesheets/manifest.css' * | xargs -i@ sed -i 's/\/stylesheets\/manifest.css/\/hot-playbook\/stylesheets\/manifest.css/g' @
 
-grep -rli '/stylesheets/print.css' * | xargs -i@ sed -i 's/\/stylesheets\/print.css/\/hot-playboook\/stylesheets\/print.css/g' @
+grep -rli '/stylesheets/print.css' * | xargs -i@ sed -i 's/\/stylesheets\/print.css/\/hot-playbook\/stylesheets\/print.css/g' @
 
-grep -rli '/stylesheets/screen.css' * | xargs -i@ sed -i 's/\/stylesheets\/screen.css/\/hot-playboook\/stylesheets\/screen.css/g' @
+grep -rli '/stylesheets/screen.css' * | xargs -i@ sed -i 's/\/stylesheets\/screen.css/\/hot-playbook\/stylesheets\/screen.css/g' @
 
-grep -rli '/stylesheets/screen-old-ie.css' * | xargs -i@ sed -i 's/\/stylesheets\/screen-old-ie.css/\/hot-playboook\/stylesheets\/screen-old-ie.css/g' @
+grep -rli '/stylesheets/screen-old-ie.css' * | xargs -i@ sed -i 's/\/stylesheets\/screen-old-ie.css/\/hot-playbook\/stylesheets\/screen-old-ie.css/g' @
 
-grep -rli '/javascripts/application.js' * | xargs -i@ sed -i 's/\/javascripts\/application.js/\/hot-playboook\/javascripts\/application.js/g' @
+grep -rli '/javascripts/application.js' * | xargs -i@ sed -i 's/\/javascripts\/application.js/\/hot-playbook\/javascripts\/application.js/g' @
 
 
 # file:\/\/\/home\/shaun\/repos\/hot-playbook\/build\/stylesheets\/manifest.css
